cc36bd31f4b9dda1943c12705e22d05e1e34092f
Oliver Costa <costaolika@gmail.com>
2024-11-22 22:16:20 +0100

    trying out cors

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -15,7 +15,16 @@ export default [
       },
     },
   },
-  "strapi::cors",
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      origin: ["http://localhost:5173", "https://solanahungary.hu"], // Add allowed origins
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"], // HTTP methods
+      headers: ["Content-Type", "Authorization"], // Allowed headers
+      keepHeadersOnError: true, // Keep headers if an error occurs
+    },
+  },
   "strapi::poweredBy",
   "strapi::query",
   "strapi::body",
