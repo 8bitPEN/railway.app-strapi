2a1f358f554722fa2d0aae75e17bb279a7ca7fd3
Oliver Costa <costaolika@gmail.com>
2024-11-22 21:32:14 +0100

    added cloudinary

--- config/plugins.ts
+++ config/plugins.ts
@@ -8,9 +8,9 @@ export default ({ env }) => ({
         config: {
             provider: 'cloudinary',
             providerOptions: {
-                cloud_name: env('dgoev1ik7'),
-                api_key: env('727677728116866'),
-                api_secret: env('3RsoOv1fG_Pk9kdF_htXd83IpTE'),
+                cloud_name: 'dgoev1ik7',
+                api_key: '727677728116866',
+                api_secret: '3RsoOv1fG_Pk9kdF_htXd83IpTE',
             },
             actionOptions: {
                 upload: {},
